3d4fcdbe446422f1c18611cfe242c569222e8cc0
tstreble <thomas.strebler@cern.ch>
2018-05-03 09:07:09 +0100

    Fixed indentation

--- L1Trigger/L1THGCal/plugins/ntuples/HGCalTriggerNtupleGenTau.cc
+++ L1Trigger/L1THGCal/plugins/ntuples/HGCalTriggerNtupleGenTau.cc
@@ -251,16 +251,16 @@ fill(const edm::Event& e, const edm::EventSetup& es)
                     for( const auto& granddaughter : granddaughters ){
 		else{
 
-		  const reco::GenParticleRefVector& granddaughters = daughter->daughterRefVector();
+                    const reco::GenParticleRefVector& granddaughters = daughter->daughterRefVector();
 
-		  for( const auto& granddaughter : granddaughters ){
+                    for( const auto& granddaughter : granddaughters ){
                         if( isStableNeutralHadron( *granddaughter ) ){
                             tau_p4vis+=(granddaughter->p4());
                             finalProds.push_back( granddaughter );
                         }
                     }
 
-		}
+                }
 
                 /* Here the selection of the decay product according to the Pythia6 decayTree */                
                 if( !isPythia8generator_ ){            
