98d0aa67635662d7cd4cb1e65b44e7b6ddd2c271
Vladimir <vladimir.rekovic@cern.ch>
2018-10-11 14:55:39 +0200

    Put simKBmtfStubs and simKBmtfDigis in the SimL1TMuon sequence, and switch input of GMT from Bmtf to KBmtf."

--- L1Trigger/L1TMuon/python/simDigis_cff.py
+++ L1Trigger/L1TMuon/python/simDigis_cff.py
@@ -25,12 +25,9 @@ SimL1TMuonCommon = cms.Sequence(simDtTriggerPrimitiveDigis + simCscTriggerPrimit
 #
 # Legacy Trigger:
 #
-<<<<<<< HEAD
-=======
 from Configuration.Eras.Modifier_stage2L1Trigger_cff import stage2L1Trigger
 from Configuration.Eras.Modifier_phase2_trigger_cff import phase2_trigger
 if not (stage2L1Trigger.isChosen() or phase2_trigger.isChosen()):
->>>>>>> Configure SimMuon sequence to run Stage2 version aslo when phase2_trigger era.
 #
 # - CSC Track Finder emulator
 #
@@ -84,9 +81,12 @@ if (stage2L1Trigger.isChosen() or phase2_trigger.isChosen()):
     from L1Trigger.L1TMuonOverlap.simOmtfDigis_cfi import *
     from L1Trigger.L1TMuon.simGmtCaloSumDigis_cfi import *
     from L1Trigger.L1TMuon.simGmtStage2Digis_cfi import *
+    from L1Trigger.L1TMuonBarrel.simKBmtfStubs_cfi import *
+    from L1Trigger.L1TMuonBarrel.simKBmtfDigis_cfi import *
+
 #
 #
-    stage2L1Trigger.toReplaceWith(SimL1TMuon, cms.Sequence(SimL1TMuonCommon + simTwinMuxDigis + simBmtfDigis + simEmtfDigis + simOmtfDigis + simGmtCaloSumDigis + simGmtStage2Digis))
+    SimL1TMuon = cms.Sequence(SimL1TMuonCommon + simTwinMuxDigis + simBmtfDigis + simKBmtfStubs + simKBmtfDigis + simEmtfDigis + simOmtfDigis + simGmtCaloSumDigis + simGmtStage2Digis)
 
 from L1Trigger.ME0Trigger.me0TriggerPseudoDigis_cff import *
 _phase2_SimL1TMuon = SimL1TMuon.copy()
