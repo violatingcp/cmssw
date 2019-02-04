f49b3b91abf135225d64dbe177267f577a7b3af8
Vladimir <vladimir.rekovic@cern.ch>
2019-02-04 08:21:13 +0100

    Phase2C4_trigger era to account for new v9 HGC geomtry, part of Phase2C4.

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -37,12 +37,13 @@ class Eras (object):
                  'Phase2_timing_layer',
                  'Phase2_timing_layer_tile',
                  'Phase2_timing_layer_bar',
+                 'Phase2_trigger',
                  'Phase2C4',
                  'Phase2C4_timing',
                  'Phase2C6',
                  'Phase2C6_timing',
                  'Phase2C4_timing_layer_bar',
-                 'Phase2_trigger',
+                 'Phase2C4_trigger',
         ]
 
         internalUseMods = ['run2_common', 'run2_25ns_specific',
